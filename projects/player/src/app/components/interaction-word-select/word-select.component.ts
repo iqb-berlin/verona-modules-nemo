89f4d048867d87310999ba82fe732e1f3796caf7
irathgen <rathgent@iqb.hu-berlin.de>
2025-07-23 21:05:43 +0200

    #19 integrate standard-button, some refactoring

--- projects/player/src/app/components/interaction-word-select/word-select.component.ts
+++ projects/player/src/app/components/interaction-word-select/word-select.component.ts
@@ -1,14 +1,20 @@
-import { Component, inject, input, OnChanges, OnDestroy, OnInit, signal } from '@angular/core';
+import {
+  Component, inject, input, OnChanges, OnDestroy, OnInit, signal
+} from '@angular/core';
 import { Response } from "@iqbspecs/response/response.interface";
 
 import { InteractionComponentDirective } from '../../directives/interaction-component.directive';
 import { WordSelectParams } from "../../models/unit-definition";
 import { ResponsesService } from "../../services/responses.service";
+import { StandardButtonComponent } from "../../shared/standard-button/standard-button.component";
 
 
 @Component({
   selector: 'stars-interaction-word-select',
   templateUrl: 'word-select.component.html',
+  imports: [
+    StandardButtonComponent
+  ],
   styleUrls: ['word-select.component.scss']
 })
 
@@ -18,14 +24,16 @@ export class WordSelectComponent extends InteractionComponentDirective implement
   responsesService = inject(ResponsesService);
 
   ngOnChanges() {
-    this.resetValues();
+    this.resetSelected();
   }
 
   ngOnInit() {
     console.log(this.parameters());
+    this.resetSelected();
     if (this.formerState()?.length > 0) {
+      // TODO: last or first element of formerState?
       const state = this.formerState()[0];
-      if (state.id === this.parameters().variableId || state.id === "WORD_SELECT") {
+      if (state.id === this.parameters().variableId || state.id === "INTERACTION_WORD_SELECT") {
         if (state.value && typeof state.value === 'number') {
           this.selectedValues.set(state.value as number);
         }
@@ -38,18 +46,18 @@ export class WordSelectComponent extends InteractionComponentDirective implement
     console.log('WordSelectComponent ngOnDestroy');
   }
 
-  isSelected(index: number): boolean {
-    return this.selectedValues() == index;
+  private resetSelected(): void {
+    this.selectedValues.set(null);
   }
 
-  resetValues(): void {
-    this.selectedValues.set(null);
+  isSelected(index: number): boolean {
+    return this.selectedValues() == index;
   }
 
   onButtonClick(index: number): void {
     this.selectedValues.set(index);
 
-    let id = this.parameters().variableId || "WORD_SELECT";
+    let id = this.parameters().variableId || "INTERACTION_WORD_SELECT";
 
     const response: Response = {
       id: id,
