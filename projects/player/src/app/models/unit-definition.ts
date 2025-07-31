4b51115aa63060c0139b9ae921c0dbc9e534473d
mechtelm <mechtel@iqb.hu-berlin.de>
2025-07-31 09:36:59 +0200

    Fix linting issues in unit-definition.ts

--- projects/player/src/app/models/unit-definition.ts
+++ projects/player/src/app/models/unit-definition.ts
@@ -1,7 +1,8 @@
-import {VariableInfo} from "./responses";
+import { VariableInfo } from './responses';
 
 export type ContinueButtonEnum = 'ALWAYS' | 'NO' | 'ON_ANY_RESPONSE' | 'ON_FULL_CREDIT' | 'ON_INTERACTION';
-export type InteractionEnum = 'BUTTONS' | 'SYLLABIFY' | 'WORD_SELECT' | 'DROP' | 'PHONETICS' | 'WRITE' | 'FIND_ON_IMAGE';
+export type InteractionEnum = 'BUTTONS' | 'SYLLABIFY' | 'WORD_SELECT' | 'DROP' |
+'PHONETICS' | 'WRITE' | 'FIND_ON_IMAGE';
 export type IconButtonTypeEnum = 'CHECK_GREEN' | 'CLOSE_RED';
 
 export interface UnitDefinition {
