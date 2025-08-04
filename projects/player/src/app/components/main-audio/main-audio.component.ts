f25cf7bb37e7d3af743de9c6a55b004c940bdf9f
irathgen <rathgent@iqb.hu-berlin.de>
2025-08-04 12:39:42 +0200

    #1 reset firstClickLayer on load new item

--- projects/player/src/app/components/main-audio/main-audio.component.ts
+++ projects/player/src/app/components/main-audio/main-audio.component.ts
@@ -1,21 +1,18 @@
 import {
-  Component, ElementRef, input, signal, ViewChild
+  Component, effect, ElementRef, inject, input, ViewChild
 } from '@angular/core';
 import { MediaPlayerComponent } from './media-player.component';
 import { ClickLayerComponent } from './click-layer.component';
 import { LinebreaksHtmlPipe } from '../../pipes/linebreaks-html.pipe';
 import { SafeResourceUrlPipe } from '../../pipes/safe-resource-url.pipe';
-import {UnitService} from "../../services/unit.service";
-import {ResponsesService} from "../../services/responses.service";
-import {VeronaPostService} from "../../services/verona-post.service";
-import {VeronaSubscriptionService} from "../../services/verona-subscription.service";
-import {MetadataService} from "../../services/metadata.service";
+import { ResponsesService } from '../../services/responses.service';
+import { MainAudio } from '../../models/unit-definition';
 
 @Component({
   selector: 'stars-main-audio',
   template: `
     <div class="audio-instruction-container">
-      @if (showLayer()) {
+      @if (showLayer) {
         <stars-click-layer (click)="layerClicked()"></stars-click-layer>
       }
       <div class="audio-button-wrapper">
@@ -26,7 +23,7 @@ import {MetadataService} from "../../services/metadata.service";
                             (elementValueChanged)="valueChanged($event)"
                             [image]="">
           <audio #player
-                 [src]="audioSource() | safeResourceUrl"
+                 [src]="localAudio.audioSource | safeResourceUrl"
                  (play)="onPlay()"
                  (pause)="onPause()"
                  (ended)="onPause()">
@@ -52,20 +49,42 @@ import {MetadataService} from "../../services/metadata.service";
   ],
   styleUrl: 'main-audio.component.css'
 })
+
 export class MainAudioComponent {
-  audioSource = input.required<string>();
+  mainAudio = input.required();
+  localAudio:MainAudio | null = null;
+
   audioText = input<string>();
   playerId = input.required<string>();
   maxPlays = input(0);
-  showLayer = signal(true);
+
+  showLayer = false;
   @ViewChild('player', { static: false }) audioElementRef!: ElementRef<HTMLAudioElement>;
   // @ViewChild('player', { static: false }) audioElement: HTMLAudioElement;
+
   private playCount: number = 0;
   isPlaying: boolean = false;
+  responsesService = inject(ResponsesService);
 
-  constructor(
-    public responsesService: ResponsesService
-  ) { }
+  constructor() {
+    effect(() => {
+      this.localAudio = this.mainAudio() as MainAudio;
+
+      if (this.localAudio) {
+        if (this.audioElementRef) {
+          this.audioElementRef.nativeElement.pause();
+          this.audioElementRef.nativeElement.currentTime = 0;
+        }
+        this.localAudio.audioSource = this.localAudio.audioSource ?
+          this.localAudio.audioSource : null;
+        if (this.localAudio.firstClickLayer) this.showLayer = true;
+        this.playCount = 0;
+        this.isPlaying = false;
+      }
+
+      console.log(this.showLayer);
+    });
+  }
 
   onPlay() {
     if (this.maxPlays() !== 0 && this.playCount >= this.maxPlays()) {
@@ -90,19 +109,9 @@ export class MainAudioComponent {
     return this.maxPlays() === 0 || this.playCount < this.maxPlays();
   }
 
-  private sendPlaybackTimeChanged() {
-    // if (this.currentTime > 0) {
-    // this.valueChange.emit({
-    //   id: this.elementModel().id,
-    //   value: this.currentTime.toString(),
-    //   status: "VALUE_CHANGED"
-    // });
-    // }
-  }
-
   layerClicked() {
     if (this.audioElementRef) this.audioElementRef.nativeElement.play();
     this.responsesService.firstInteractionDone.set(true);
-    this.showLayer.set(false);
+    this.showLayer = false;
   }
 }
