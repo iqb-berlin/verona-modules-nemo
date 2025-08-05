f8c1238c98e66c39bc9c4cfa8477a8b45cf5b7d0
irathgen <rathgent@iqb.hu-berlin.de>
2025-08-05 12:40:58 +0200

    #1 animation running on play/pause

--- projects/player/src/app/app.module.ts
+++ projects/player/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
 
 import { AppComponent } from './app.component';
 import { InteractionButtonsComponent } from './components/interaction-buttons/interaction-buttons.component';
@@ -50,7 +52,10 @@ import { InteractionDropComponent } from './components/interaction-drop/interact
     ResponsesService,
     VeronaPostService,
     VeronaSubscriptionService,
-    MetadataService
+    MetadataService,
+    provideLottieOptions({
+      player: () => player
+    })
   ],
   bootstrap: [AppComponent]
 })
--- projects/player/src/app/components/main-audio/main-audio.component.ts
+++ projects/player/src/app/components/main-audio/main-audio.component.ts
@@ -3,7 +3,6 @@ import {
 } from '@angular/core';
 import { MediaPlayerComponent } from './media-player.component';
 import { ClickLayerComponent } from './click-layer.component';
-import { LinebreaksHtmlPipe } from '../../pipes/linebreaks-html.pipe';
 import { SafeResourceUrlPipe } from '../../pipes/safe-resource-url.pipe';
 import { ResponsesService } from '../../services/responses.service';
 import { MainAudio } from '../../models/unit-definition';
@@ -20,8 +19,7 @@ import { MainAudio } from '../../models/unit-definition';
                             [playerId]="playerId()"
                             [isPlaying]="isPlaying"
                             [disabled]="canPlay()"
-                            (elementValueChanged)="valueChanged($event)"
-                            [image]="">
+                            (elementValueChanged)="valueChanged($event)">
           <audio #player
                  [src]="localAudio.audioSource | safeResourceUrl"
                  (play)="onPlay()"
@@ -33,18 +31,11 @@ import { MainAudio } from '../../models/unit-definition';
           </label>
         </stars-media-player>
       </div>
-
-      @if (audioText() && audioText().trim()) {
-        <div class="audio-text-wrapper">
-          <p class="audio-instruction-text" [innerHTML]="audioText() | lineBreaksHtml"></p>
-        </div>
-      }
     </div>
   `,
   imports: [
     MediaPlayerComponent,
     ClickLayerComponent,
-    LinebreaksHtmlPipe,
     SafeResourceUrlPipe
   ],
   styleUrl: 'main-audio.component.css'
@@ -54,7 +45,6 @@ export class MainAudioComponent {
   mainAudio = input.required();
   localAudio:MainAudio | null = null;
 
-  audioText = input<string>();
   playerId = input.required<string>();
   maxPlays = input(0);
 
@@ -81,8 +71,6 @@ export class MainAudioComponent {
         this.playCount = 0;
         this.isPlaying = false;
       }
-
-      console.log(this.showLayer);
     });
   }
 
@@ -106,7 +94,8 @@ export class MainAudioComponent {
   }
 
   canPlay(): boolean {
-    return this.maxPlays() === 0 || this.playCount < this.maxPlays();
+    return true;
+    //return this.maxPlays() === 0 || this.playCount < this.maxPlays();
   }
 
   layerClicked() {
