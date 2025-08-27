a908dcabb5b7b39c1c6c09263cc69682df0eb9f1
Irem Peker <irem.peker@gmx.de>
2025-08-27 17:14:22 +0200

    #94 Add missing lines

--- projects/player/src/app/components/ribbon-bar/ribbon-bar.component.ts
+++ projects/player/src/app/components/ribbon-bar/ribbon-bar.component.ts
@@ -1,6 +1,7 @@
 import {
-  Component, computed, input
+  Component, computed, inject
 } from '@angular/core';
+import { UnitService } from '../../services/unit.service';
 
 @Component({
   selector: 'stars-ribbon-bar',
@@ -11,10 +12,13 @@ import {
   styleUrls: ['./ribbon-bar.component.scss']
 })
 export class RibbonBarComponent {
-  backgroundColor = input('white');
+  private unitService = inject(UnitService);
+
+  // Get background color from the service instead of input
+  backgroundColor = computed(() => this.unitService.backgroundColor());
 
   ribbonClass = computed(() => {
     const bgColorUpper = this.backgroundColor().toUpperCase();
-    return ['#FFF', '#FFFFFF', '#EEE', '#EEEEEE', 'WHITE'].includes(bgColorUpper) ? 'ribbon-bar-white' : 'ribbon-bar';
+    return ['#FFF', '#FFFFFF', '#EEE', '#EEEEEE', 'WHITE'].includes(bgColorUpper) ? 'ribbon-bar' : 'ribbon-bar-white';
   });
 }
