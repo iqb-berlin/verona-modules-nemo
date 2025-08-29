d4bd2019a17f7598fdb3fdc666bfdf2671cf4798
mechtelm <mechtel@iqb.hu-berlin.de>
2025-08-29 12:29:08 +0200

    Fix wimmel check area and coding

--- projects/player/src/app/components/interaction-find-on-image/find-on-image.component.ts
+++ projects/player/src/app/components/interaction-find-on-image/find-on-image.component.ts
@@ -34,17 +34,18 @@ export class InteractionFindOnImageComponent extends InteractionComponentDirecti
         this.localParameters.text = parameters.text || '';
         this.localParameters.showArea = parameters.showArea || '';
         this.localParameters.size = parameters.size || 'SMALL';
-        if (this.localParameters?.showArea) {
+        if (this.localParameters.showArea) {
           const area = this.localParameters.showArea.match(/\d+/g);
+          const imgWidth = this.imageRef.nativeElement.width;
+          const imgHeight = this.imageRef.nativeElement.height;
+          const imgTop = this.imageRef.nativeElement.offsetTop;
+          const imgLeft = this.imageRef.nativeElement.offsetLeft;
 
-          const xMultiplier = this.imageRef.nativeElement.width / 100;
-          const yMultiplier = this.imageRef.nativeElement.height / 100;
-
-          const x1 = Math.round(Number.parseInt(area[0], 10) * xMultiplier);
-          const y1 = Math.round(Number.parseInt(area[1], 10) * yMultiplier);
-          const x2 = Math.round(Number.parseInt(area[2], 10) * xMultiplier);
-          const y2 = Math.round(Number.parseInt(area[3], 10) * yMultiplier);
-
+          const x1 = Math.round((Number.parseInt(area[0], 10) * (imgWidth / 100)) + imgLeft);
+          const y1 = Math.round((Number.parseInt(area[1], 10) * (imgHeight / 100)) + imgTop);
+          const x2 = Math.round((Number.parseInt(area[2], 10) * (imgWidth / 100)) + imgLeft);
+          const y2 = Math.round((Number.parseInt(area[3], 10) * (imgHeight / 100)) + imgTop);
+console.log(`x1: ${x1} y1: ${y1} x2: ${x2} y2: ${y2}`);
           this.showAreaStyle.set(`top: ${y1}px; left: ${x1}px; width: ${x2 - x1}px; height: ${y2 - y1}px;`);
         }
         this.responses.emit([{
@@ -58,14 +59,14 @@ export class InteractionFindOnImageComponent extends InteractionComponentDirecti
   }
 
   onClick(event) {
-    if (this.buttonDisabled()) this.buttonDisabled.set(false);
     this.clickTargetLeft.set(`${event.layerX}px`);
     this.clickTargetTop.set(`${event.layerY}px`);
+    if (this.buttonDisabled()) this.buttonDisabled.set(false);
+
     const imgWidth = this.imageRef.nativeElement.width;
     const imgHeight = this.imageRef.nativeElement.height;
     const imgTop = this.imageRef.nativeElement.offsetTop;
     const imgLeft = this.imageRef.nativeElement.offsetLeft;
-
     const x = Math.round(((event.layerX - imgLeft) / imgWidth) * 100);
     const y = Math.round(((event.layerY - imgTop) / imgHeight) * 100);
 
