964db059fd0c5f7fb99f04c80cad6908c930fabf
Yan Ping Chen Lin <yanpingchenlin@gmail.com>
2025-09-02 09:53:35 +0200

    #126 add test for continue button

--- cypress/e2e/template.spec.cy.ts
+++ cypress/e2e/template.spec.cy.ts
@@ -8,17 +8,24 @@ describe('Checkbox element', { testIsolation: false }, () => {
 
     it('clicks at the ear button', () => {
       cy.get('.layer').click();
+      // write a test that checks that the sound is played
     });
 
-    it('types the word Kopf', () => {
+    it('checks that the continue button is not active', () => {
+      cy.get('stars-continue-button').should('not.exist');
+    });
+
+    it('types the word Kopf, and checks that the text display contains the word', () => {
       cy.get('[aria-label="K"]').click();
       cy.get('[aria-label="o"]').click();
       cy.get('[aria-label="p"]').click();
       cy.get('[aria-label="f"]').click();
+
+      cy.get('.text-display').should('contain', 'Kopf');
     });
 
-    it('checks that the text dispklay contains the word Kopf', () => {
-      cy.get('.text-display').should('contain', 'Kopf');
+    it('checks that the continue button is now active', () => {
+      cy.get('stars-continue-button').should('exist');
     });
   });
 });
