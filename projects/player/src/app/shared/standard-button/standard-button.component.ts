48c50821fa84952f6c17c02927f9781296d6f8eb
Irem Peker <irem.peker@gmx.de>
2025-10-20 16:53:47 +0200

    #157 Remove commented out lines

--- projects/player/src/app/shared/standard-button/standard-button.component.ts
+++ projects/player/src/app/shared/standard-button/standard-button.component.ts
@@ -3,7 +3,7 @@ import {
 } from '@angular/core';
 
 import { ButtonTypeEnum } from '../../models/unit-definition';
-import { StandardIconComponent } from "../standard-icon/standard-icon.component";
+import { StandardIconComponent } from '../standard-icon/standard-icon.component';
 
 @Component({
   selector: 'stars-standard-button',
@@ -31,33 +31,6 @@ export class StandardButtonComponent {
   textMode = computed(() => {
     return !!this.text() && !this.icon() && !this.image();
   });
-
-  // className = computed(() => {
-  //   const base = `${((this.type() || '') as string).toLowerCase()  }-type`;
-  //   // if it's a circle, prefer 'repeat' class when repeatButtons is truthy,
-  //   // otherwise add svg-icon only if the icon is SVG.
-  //   if (this.type() === 'CIRCLE') {
-  //     // eslint-disable-next-line no-nested-ternary
-  //     return base + (this.repeatButtons() ? '-repeat' : (this.isSvgIcon() ? '-svg-icon' : ''));
-  //   }
-  //   return base + (this.isSvgIcon() ? ' svg-icon' : '');
-  // });
-  //
-  // // safely detect if the current icon represents an SVG (inline SVG or .svg file URL)
-  // isSvgIcon(): boolean {
-  //   try {
-  //     const iconVal = this.icon ? this.icon() : null;
-  //     if (!iconVal || typeof iconVal !== 'string') return false;
-  //     const trimmed = iconVal.trim();
-  //
-  //     // treat keys like SMILEY_1, SMILEY_2, ... as SVG sprite/icon identifiers
-  //     if (/^smiley_\d+$/i.test(trimmed)) return true;
-  //
-  //     return false;
-  //   } catch {
-  //     return false;
-  //   }
-  // }
 
   onClick(): void {
     this.buttonClick.emit();
